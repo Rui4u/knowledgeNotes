f4f3b6cb03ba386fe23b079f85f5a79396b34907
sharui <sr_sharui@qq.com>
2019-12-05 15:02:47 +0800

    update:添加首页逻辑判断

--- Web/Code/TravleProduct/js/list.js
+++ Web/Code/TravleProduct/js/list.js
@@ -1,17 +1,13 @@
 ;
 window.addEventListener('load', function() {
 
-    var contain = this.document.querySelector('.contains');
-    var itemdiv = contain.querySelector('div')
-
     var titleArray = ["航班信息", "行程安排", "出行须知", "景点介绍", "酒店信息", "奇幻之夜", "退税说明", "趣味活动", "精彩瞬间"];
-    for (let index = 0; index < itemdiv.children.length; index++) {
+    var spanArray = $(".contains div span");
+    for (let index = 0; index < spanArray.length; index++) {
 
-        var item = itemdiv.children[index];
-        var label = item.children[1];
-
-        label.innerText = titleArray[index];
-        this.console.log(label)
+        var item = spanArray[index];
+        var label = $(item).children("label");
+        label[0].innerText = titleArray[index];
     }
 
 
