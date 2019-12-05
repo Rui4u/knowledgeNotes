8c23cc9b48c80f4eebeed4ea1aaa7b9c4de153ca
sharui <sr_sharui@qq.com>
2019-12-05 17:03:10 +0800

    update:修复代码

--- Web/Code/TravleProduct/js/list.js
+++ Web/Code/TravleProduct/js/list.js
@@ -2,17 +2,28 @@
 window.addEventListener('load', function() {
 
     var titleArray = ["航班信息", "行程安排", "出行须知", "景点介绍", "酒店信息", "奇幻之夜", "退税说明", "趣味活动", "精彩瞬间"];
-    var spanArray = $(".contains div");
-    spanArray = spanArray.children("span")
-    for (let index = 0; index < spanArray.length; index++) {
+    $(".contains div").children("span").each(function(index, element) {
+        $(element).children("label").html(titleArray[index]);
+        $(element).find("img").prop("src", "img/list/item_image_" + index + ".png ");
+    });
 
-        var item = spanArray[index];
-        var label = $(item).children("label");
-        label[0].innerText = titleArray[index];
 
-        var img = $(item).find("img");
-        img.prop("src", "img/list/item_image_" + index + ".png ");
-    }
+
+    // var spanArray = $(".contains div");
+
+    // spanArray = spanArray.children("span")
+
+
+    // for (let index = 0; index < spanArray.length; index++) {
+
+    //     var item = spanArray[index];
+    //     $(item).children("label").html(titleArray[index]);
+    //     // label[0].innerText = titleArray[index];
+
+    //     var img = $(item).find("img");
+
+    //     img.prop("src", "img/list/item_image_" + index + ".png ");
+    // }
 
 
 });
