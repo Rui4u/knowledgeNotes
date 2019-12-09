a94f73adfbd4f9f8d3eadc3ebc211b85951515f1
sharui <sr_sharui@qq.com>
2019-12-09 19:02:31 +0800

    update:更新接口

--- Python/Flask-Travel/app.py
+++ Python/Flask-Travel/app.py
@@ -1,7 +1,9 @@
+from builtins import *
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +14,13 @@ app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
+
+class api_request_type():
+    successed = 200
+    failure = -200
+    user_cant_find = -502
+    user_cant_find_by_id = -503
+    failure_no_pars = -504
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -46,6 +55,123 @@ class Flights(db.Model):
 
     def __repr__(self):
         return 'User:%s ' % self.flight_name
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+@app.route('/')
+def hello_world():
+    return '123'
+
+
+@app.route('/login')
+def login():
+    id = request.args.get("id") or ''
+    phone = request.args.get("phone") or ''
+    name = request.args.get("name") or ''
+    print(id)
+    print(phone)
+    print(name)
+
+    json = backjson(find(id=id, phone=phone, name=name))
+    return json
+
+
+@app.route('/flight')
+def flight():
+    id = request.args.get("id") or ''
+    print(id)
+
+    json = backjson(findFlights(id))
+    return json
+
+
+def serialize(model):
+    from sqlalchemy.orm import class_mapper
+    columns = [c.key for c in class_mapper(model.__class__).columns]
+    dicts = dict((c, getattr(model, c)) for c in columns)
+    print('数据库转字典 %s' % (dicts))
+    return dicts
+
+
+def backjson(resp):
+
+    print('开始解析')
+
+    data = {}
+    req_status = api_request_type.failure
+    message = ''
+
+    if isinstance(resp, db.Model):
+        data = serialize(resp)
+        req_status = api_request_type.successed
+    elif type(resp)is dict:
+        data = resp
+        req_status = api_request_type.successed
+    elif isinstance(resp, int):
+        data = {}
+        req_status = resp
+    else:
+        data = {}
+        req_status = api_request_type.failure
+
+     #判断状态码 返回message
+    if req_status == api_request_type.successed:
+        message = '操作成功'
+    elif req_status == api_request_type.user_cant_find:
+        message = '此姓名、手机号无对应嘉宾,请联系管理员'
+    elif req_status == api_request_type.user_cant_find_by_id:
+        message = '此id无对应嘉宾,请联系管理员'
+    elif req_status == api_request_type.failure_no_pars:
+        message = '参数错误'
+    elif req_status == api_request_type.failure:
+        message = '操作失败'
+    else:
+        message = '未知错误'
+
+    deal_resp = {
+        'meta': {'status': req_status, 'message': message},
+        'data': data
+            }
+    json = jsonify(deal_resp)
+    print(deal_resp)
+    print('结束解析')
+
+    return json
+
+
+################初始化数据
+@app.route('/initdb', methods=['GET'])
+def initdb():
+    # 全部删除
+    db.drop_all()
+    # 创建
+    add(name='王0', sex='男', phone=12222222222)
+    add(name='王1', sex='女', phone=13333333333)
+    user = find(name='王0')
+    addFlights(flight_name='阿联酋航空EK309',
+               flight_status=0,
+               flight_type='团队出发',
+               flight_luggage_rules='行李规则',
+               departure_city='北京首都T2',
+               departure_date='2019/04/03',
+               departure_time='07:25',
+               arrival_city='迪拜T3',
+               arrival_date='2019/04/04',
+               arrival_time='11:35',
+               user_id=user.id)
+    user = find(name='王0')
+    print('--')
+    flight = user.flight[0]
+    q_dict = serialize(flight)
+    q_json = jsonify(q_dict)
+    str1 = 'q_dict = %s \n q_json = %s' % (q_dict, q_json)
+    print(str1)
+    print('--')
+
+    return q_json
 
 
 def addFlights(flight_name,
@@ -86,45 +212,7 @@ def addFlights(flight_name,
     else:
         print('名字为空')
 
-@app.route('/')
-def hello_world():
-    return '123'
-
-
-if __name__ == '__main__':
-    app.run()
-
-
-@app.route('/adds', methods=['GET'])
-def adds():
-
-    db.drop_all()
-    add(name='王0', sex='男', phone=12222222222)
-    add(name='王1', sex='女', phone=13333333333)
-    user = find(name='王0')
-    addFlights(flight_name='阿联酋航空EK309',
-               flight_status=0,
-               flight_type='团队出发',
-               flight_luggage_rules='行李规则',
-               departure_city='北京首都T2',
-               departure_date='2019/04/03',
-               departure_time='07:25',
-               arrival_city='迪拜T3',
-               arrival_date='2019/04/04',
-               arrival_time='11:35',
-               user_id=user.id)
-    user = find(name='王0')
-    print('--')
-    flight = user.flight[0]
-    q_dict = serialize(flight)
-    q_json = jsonify(q_dict)
-    str1 = 'q_dict = %s \n q_json = %s' % (q_dict, q_json)
-    print(str1)
-    print('--')
-
-    return q_json
-
-
+#############数据库操作
 def add(name, sex=None, phone=None):
     if len(name):
         try:
@@ -142,33 +230,48 @@ def add(name, sex=None, phone=None):
         print('名字为空')
 
 
-def find(name=None):
+def find(name=None, phone=None, id=None):
 
-    try:
-        user = User.query.filter_by(name=name).first()
-        print(user.id)
-        return user
+    if len(id) > 0 & int(id) > 0:
+        try:
+            user = User.query.filter_by(id=id).first()
+            print(user.id)
+            return user
 
-    except Exception as e:
-        print(e)
-        print('查找出错')
-        db.session.rollback()
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return api_request_type.user_cant_find_by_id
+    elif len(name) > 0 and len(phone) > 0 :
+        try:
+            user = User.query.filter_by(phone=phone).filter_by(name=name).first()
+            print(user.id)
+            return user
+
+        except Exception as e:
+            print(e)
+
+            db.session.rollback()
+            return api_request_type.user_cant_find
+    else:
+        return api_request_type.failure_no_pars
 
 
-def findFlights(name=None):
+def findFlights(id):
+    if(len(id) > 0):
+        try:
+            flight = Flights.query.filter_by(user_id=id).first()
+            return flight
 
-    try:
-        flight = Flights.query.filter_by().first()
-        print(flight.id)
-        print(flight.user_id)
-        return flight
-
-    except Exception as e:
-        print(e)
-        print('查找出错')
-        db.session.rollback()
+        except Exception as e:
+            print(e)
+            print('查找出错')
+            db.session.rollback()
+    else:
+        return '参数出错'
 
 def delete(id):
+
     try:
         user = User.query.filter_by(id=28).delete()
         db.session.commit()
@@ -189,7 +292,7 @@ def change():
         db.session.rollback()
 
 
-def deleteAll():
+def deleteall():
     try:
         user = User.query.delete()
         db.session.commit()
@@ -197,10 +300,3 @@ def deleteAll():
         print(e)
         print('删除出错')
         db.session.rollback()
-
-
-
-def serialize(model):
-    from sqlalchemy.orm import class_mapper
-    columns = [c.key for c in class_mapper(model.__class__).columns]
-    return dict((c, getattr(model, c)) for c in columns)
